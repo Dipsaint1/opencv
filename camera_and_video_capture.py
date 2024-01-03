e653c29c57de0610d65569aa79b8b7248b329a48
Cyber9ja <ayodejioladapo96@gmail.com>
2024-01-03 13:27:29 +0000

    minor update

--- camera_and_video_capture.py
+++ camera_and_video_capture.py
@@ -2,22 +2,20 @@ import numpy as np;
 import cv2;
 
 # Define a variable to access the webcam
-camera_and_video_capture = cv2.VideoCapture(0);
+camera_and_video_capture = cv2.VideoCapture(0);  
 
 def open_frame():
   while True:
-    ret, frame = camera_and_video_capture.read();
-    cv2.imshow('frame',frame);
-    
+    ret, frame = camera_and_video_capture.read();  #Open the webcam
+
+    image = np.zeros(frame.shape, np.uint8); #Return an array
+
+    cv2.imshow('frame', image);
     if(cv2.waitKey(1) & 0xFF == ord('q')):  #press q to quit frame
       break;                                                                                
 
-# while True :
-#   ret, frame = camera_and_video_capture.read();
-#   cv2.imshow('frame',frame);
-#   if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit frame
-#     break;
-
 open_frame();
-camera_and_video_capture.release();
+
+# When everything done, release the capture
+camera_and_video_capture.release(); 
 cv2.destroyAllWindows();
